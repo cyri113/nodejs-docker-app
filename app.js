da8c8ff55a05f1456d26dc6a663c2c8e2a060926
Cyrille <c.derche@me.com>
2019-05-24 16:18:31 +0200

    log Hello World

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
+    console.log('Hello World!')
     res.status(200).send('Hello World!')
 })
 module.exports = app
